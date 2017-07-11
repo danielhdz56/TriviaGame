d0626376a496d33244875a7f4bdb0df48bcbe8e8
Daniel Hernandez <daniel56hdz@gmail.com>
2017-07-11 18:53:54 -0500

    Added Questions from API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,28 +1,38 @@
 $('#startButton').on('click', function(){
 	$('#initial').remove();
-	trivia.start();
+	trivia.token();
 });
 var intervalId;
 var countdownRunning = false;
+var myToken;
+var counter = 0;
+var questions;
 
 var trivia = {
-	time: 30,
+	time: 5,
+	token: function() {
+		//The purpose of the session token is to not given the player the same questions once he has lost/won
+		//This works by helping the server keep track of the questions that have already been requested
+		var requestToken = "https://opentdb.com/api_token.php?command=request";
+		$.get(requestToken, function(data) {
+			myToken = data.token;
+			trivia.pull();
+			trivia.start();
+		});
+	},
+	pull: function() {
+		//I am using the session token load all the questions
+		var useToken = "https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple&token=" + myToken;
+		$.get(useToken, function(data){
+			questions = data.results;
+			console.log(questions);
+		});
+	},
 	start: function() {
 		countdownRunning: false;
 		if(!countdownRunning) {
-			$('#timer').html('Time Remaining: 30 Seconds');
-			intervalId = setInterval(trivia.count, 1000);
-   			//The first step is to request a session token fromt the API i'm using
-   			var requestToken = "https://opentdb.com/api_token.php?command=request";
-   			$.get(requestToken, function(data) {
-   				var token = data.token;
-   				console.log(token);				
-   				//Now I want to use the session token
-   				var useToken = "https://opentdb.com/api.php?amount=10&token=" + token;
-   				$.get(useToken, function(data){
-   					console.log(data.results[0]);
-   				});
-   			});
+			$('#timer').html('Time Remaining: 5 Seconds');
+			intervalId = setInterval(trivia.count, 1000);	
 		}
 	},
 	count: function() {
@@ -30,8 +40,12 @@ var trivia = {
 		$('#timer').html('Time Remaining: ' + trivia.time + ' Seconds');
 		if(trivia.time === 0) {
 			clearInterval(intervalId);
-			console.log("newQuestion");
-			trivia.time = 30;
+			//By specifying the questions.length instead of an exact number I can still use this even when I remove a question from the array
+			var randomNumber = Math.floor(Math.random()*questions.length);
+			console.log(questions[randomNumber]);
+			questions.splice(randomNumber, 1);
+			console.log(questions);
+			trivia.time = 5;
 			trivia.start();
 		}
 	}
